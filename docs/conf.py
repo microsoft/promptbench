20ef93238323b30d6a66b392cd5416a2b12d8a69
Immortalise <kaijiezhu11@gmail.com>
2023-12-06 01:11:54 -0800

    [fix] table html presentation

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,6 @@ import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 
 
-
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'promptbench'
@@ -63,6 +60,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
+
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
